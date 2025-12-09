2822ec057c9d323e58617327a934c11d7f41f8ef
Minh Truong <truongbinhminh27122006@gmail.com>
2025-12-09 11:30:24 +0700

    Add files via upload

---
+++ midterm.cpp
@@ -0,0 +1,753 @@
+﻿#include <iostream>
+#include <fstream>
+#include <stack>
+#include <queue>
+using namespace std;
+
+
+struct Graph {
+	int** matrix;
+	int** list;
+	int n;
+	int* sz;
+	int type;
+	int ts;
+	int dir ;
+	int* canuse;
+	int** check;
+	int* visited;
+	int* parent;
+	int* label;
+};
+
+
+void init(Graph& m) {
+	m.matrix = nullptr;
+	m.list = nullptr;
+	m.sz = nullptr;
+	m.canuse = nullptr;
+	m.check = nullptr;
+	m.visited = nullptr;
+	m.parent = nullptr;
+	m.label = nullptr;
+	m.dir = 0;
+}
+
+void createGraph(Graph& m) {
+	cout << "Nhap so dinh: ";
+	cin >> m.n;
+	switch (m.type)
+	{
+	case 1:
+	{
+		m.matrix = new int* [m.n];
+		for (int i = 0; i < m.n; i++) {
+			m.matrix[i] = new int[m.n];
+		}
+		for (int i = 0; i < m.n; i++) {
+			for (int j = 0; j < m.n; j++) {
+				m.matrix[i][j] = 0;
+			}
+		}
+		break;
+	}
+	case 0:
+	{
+		m.list = new int* [m.n];
+		m.sz = new int[m.n];
+		for (int i = 0; i < m.n; i++) {
+			m.list[i] = nullptr;
+			m.sz[i] = 0;
+		}
+
+		break;
+	}
+	default:
+		break;
+	}
+	m.canuse = new int[m.n];
+	m.visited = new int[m.n];
+	m.parent = new int[m.n];
+	m.label = new int[m.n];
+	for (int i = 0; i < m.n; i++) {
+		m.canuse[i] = 1;
+		m.label[i] = 0;
+		m.parent[i] = 0;
+		m.visited[i] = 0;
+	}
+	m.check = new int*[m.n];
+	for (int i = 0; i < m.n; i++) {
+		m.check[i] = new int[m.n];
+	}
+	for (int i = 0; i < m.n; i++) {
+		for (int j = 0; j < m.n; j++) {
+			m.check[i][j] = 0;
+		}
+	}
+
+}
+
+void output(Graph m) {
+	cout << "DO THI HIEN TAI \n";
+	switch (m.type)
+	{
+	case 1: {
+		for (int i = 0; i < m.n; i++) {
+			if (m.canuse[i] == 0)continue;
+			cout << "Dinh " << i + 1 << ": ";
+			for (int j = 0; j < m.n; j++) {
+				if (m.canuse[j] == 1) {
+					cout << m.matrix[i][j] << " ";
+				}
+			}
+			cout << endl;
+		}
+		break;
+	}
+	case 0: {
+		for (int i = 0; i < m.n; i++) {
+			if (m.canuse[i] == 0)continue;
+			cout << "Dinh " << i + 1 << ": ";
+			for (int j = 0; j < m.sz[i]; j++) {
+				int dinh = m.list[i][j];
+				if (m.canuse[dinh] == 1) {
+					cout << dinh + 1<< " ";
+				}
+			}
+			cout << endl;
+		}
+		break;
+	}
+	default:
+		break;
+	}
+
+}
+
+
+
+
+void addVertex(Graph& m, int v) {
+	if (v == 0)return;
+	int newsize = m.n + v;
+	switch (m.type)
+	{
+	case 1: {
+		int** newmatrix = new int* [newsize];
+		for (int i = 0; i < newsize; i++) {
+			newmatrix[i] = new int[newsize];
+		}
+
+		for (int i = 0; i < newsize; i++) {
+			for (int j = 0; j < newsize; j++) {
+				newmatrix[i][j] = 0;
+			}
+		}
+		for (int i = 0; i < m.n; i++) {
+			for (int j = 0; j < m.n; j++) {
+				newmatrix[i][j] = m.matrix[i][j];
+			}
+		}
+		for (int i = 0; i < m.n; i++) {
+			delete[]m.matrix[i];
+		}
+		delete[]m.matrix;
+		m.matrix = newmatrix;
+
+		break;
+	}
+	case 0: {
+		int** newlist = new int* [newsize];
+		for (int i = 0; i < newsize; i++) {
+			newlist[i] = nullptr;
+		}
+		for (int i = 0; i < m.n; i++) {
+			for (int j = 0; j < m.sz[i]; j++) {
+				newlist[i][j] = m.list[i][j];
+			}
+		}
+		for (int i = 0; i < m.n; i++) {
+			delete[]m.list[i];
+		}
+		delete[]m.list;
+		m.list = newlist;
+
+		int* newsz = new int[newsize];
+		for (int i = 0; i < newsize; i++) {
+			newsz[i] = 0;
+		}
+
+		for (int i = 0; i < m.n; i++) {
+			newsz[i] = m.sz[i];
+		}
+		delete[]m.sz;
+		m.sz = newsz;
+
+		break;
+	}
+	default:
+		break;
+	}
+	int* newcanuse = new int[newsize];
+	for (int i = 0; i < newsize; i++) {
+		newcanuse[i] = 1;
+	}
+	for (int i = 0; i < m.n; i++) {
+		newcanuse[i] = m.canuse[i];
+	}
+	delete[]m.canuse;
+	m.canuse = newcanuse;
+
+	int** newcheck = new int*[newsize];
+	for (int i = 0; i < newsize; i++) {
+		newcheck[i] = new int[newsize];
+	}
+	for (int i = 0; i < newsize; i++) {
+		for (int j = 0; j < newsize; j++) {
+			newcheck[i][j] = 0;
+		}
+	}
+	for (int i = 0; i < m.n; i++) {
+		for (int j = 0; j < m.n; j++) {
+			newcheck[i][j] = m.check[i][j];
+		}
+	}
+	for (int i = 0; i < m.n; i++) {
+		delete[]m.check[i];
+	}
+	delete[]m.check;
+	m.check = newcheck;
+
+	m.n = newsize;
+
+}
+
+void delVertex(Graph& m, int v) {
+	if (v < 0 && v > m.n) {
+		cout << "Vuot pham vi\n";
+		return;
+	}
+	if (m.canuse[v] == 1) {
+		m.canuse[v] = 0;
+	}
+	else {
+		cout << "DInh nay da bi xoa \n";
+	}
+}
+
+void push(int*& a, int& n, int x)
+{
+	if (a == nullptr) {
+		n = 1;
+		a = new int[n];
+		a[0] = x;
+		return;
+	}
+	int* newarr = new int[n + 1];
+	for (int i = 0; i < n; i++) {
+		newarr[i] = a[i];
+	}
+	newarr[n] = x;
+	delete[]a;
+	a = newarr;
+	n++;
+
+}
+
+void addEdge(Graph& m, int a, int b) {
+	if (m.canuse[a] == 0 || m.canuse[b] == 0) {
+		cout << "Khong the them canh vao do thi\n";
+		return;
+	}
+
+	switch (m.type)
+	{
+	case 1: {
+		if (m.ts == 1) {
+			int ts;
+			cout << "Nhap trong so cho canh " << a + 1 << "->" << b + 1 << ": ";
+			cin >> ts;
+			m.matrix[a][b] = ts;
+			m.matrix[b][a] = ts;
+		}
+		else {
+
+			m.matrix[a][b] = 1;
+			m.matrix[b][a] = 1;
+		}
+
+		break;
+	}
+
+	case 0: {
+		if (m.dir == 0) {
+			push(m.list[a], m.sz[a], b);
+			push(m.list[b], m.sz[b], a);
+		}
+		else {
+			push(m.list[a], m.sz[a], b);
+		}
+		break;
+	}
+
+	default:
+		break;
+	}
+
+}
+
+
+void pop(int*& a, int& n, int x) {
+	if (n == 1) {
+		a = nullptr;
+		n = 0;
+		return;
+	}
+	int* newarr = new int[n - 1];
+	int idx = 0;
+	for (int i = 0; i < n; i++) {
+		if (a[i] == x)continue;
+		newarr[idx] = a[i];
+		idx++;
+	}
+	delete[]a;
+	a = newarr;
+	n--;
+}
+
+void delEdge(Graph& m, int a, int b) {
+	if (m.canuse[a] == 0 || m.canuse[b] == 0) {
+		cout << "Khong the xoa canh\n";
+		return;
+	}
+
+	switch (m.type)
+	{
+	case 1: {
+		m.matrix[a][b] = 0;
+		m.matrix[b][a] = 0;
+
+		break;
+	}
+
+	case 0: {
+
+		if (m.dir == 1) {
+			pop(m.list[a], m.sz[a], b);
+		}
+		else {
+			pop(m.list[a], m.sz[a], b);
+			pop(m.list[b], m.sz[b], a);
+		}
+
+		break;
+	}
+
+	default:
+		break;
+	}
+
+}
+
+
+void del(Graph& m) {
+	for (int i = 0; i < m.n; i++) {
+		if (m.matrix != nullptr) {
+			delete[]m.matrix[i];
+		}
+		if (m.list != nullptr) {
+			delete[]m.list[i];
+		}
+		if(m.check!=nullptr){
+			delete[]m.check[i];
+		}
+	}
+	delete[]m.check;
+	delete[]m.sz;
+	delete[]m.list;
+	delete[]m.matrix;
+	delete[]m.canuse;
+	delete[]m.visited;
+	delete[]m.parent;
+	delete[]m.label;
+	m.label = nullptr;
+	m.visited = nullptr;
+	m.parent = nullptr;
+	m.canuse = nullptr;	
+	m.sz = nullptr;
+	m.list = nullptr;
+	m.matrix = nullptr;
+	m.check = nullptr;
+	bool res = m.sz == nullptr && m.list == nullptr && m.matrix == nullptr && m.canuse == nullptr && m.visited == nullptr && m.parent == nullptr && m.label == nullptr ? true : false;
+	if (res) {
+		cout << "Giai phong thanh cong \n";
+	}
+}
+
+
+int calMaxCanh(int n) {
+	if (n == 1) {
+		return 0;
+	}
+	if (n == 2) {
+		return 1;
+	}
+	if (n == 3) {
+		return 3;
+	}
+	n--;
+	int res = 1;
+	for (int i = 1; i <= n; i++) {
+		res = res * i;
+	}
+	return res;
+}
+
+int isGraph(Graph& m) {
+	for (int i = 0; i < m.n; i++) {
+		for (int j = 0; j < m.n; j++) {
+			m.check[i][j] = 0;
+		}
+	}
+
+	int res = 0;
+	switch (m.type)
+	{
+	case 1: {
+		for (int i = 0; i < m.n; i++) {
+			if (m.canuse[i] == 0)continue;
+			for (int j = 0; j < m.n; j++) {
+				if (m.matrix[i][j] > 0 && m.check[j][i] == 0 && m.canuse[j]==1) {
+					res++;
+					m.check[i][j] = 1;
+				}
+			}
+		}
+		break;
+	}
+	case 0: {
+		for (int i = 0; i < m.n; i++) {
+			if (m.canuse[i] == 0)continue;
+			for (int j = 0; j < m.sz[i]; j++) {
+				int dinh = m.list[i][j];
+				if ( m.check[dinh][i] == 0 && m.canuse[dinh] == 1) {
+					res++;
+					m.check[i][dinh] = 1;
+				}
+			}
+		}
+		break;
+	}
+	default:
+		break;
+	}
+
+	if (res <= calMaxCanh(m.n)) {
+		return res;
+	}
+	else {
+		return -1;
+	}
+}
+
+
+void duyet(Graph& m, int start, int label) {
+	for (int i = 0; i < m.n; i++) {
+		m.visited[i] = 0;
+	}
+	stack<int>s;
+	s.push(start);
+	while (!s.empty())
+	{
+		int r = s.top();
+		m.visited[r] = 1;
+		m.label[r] = label;
+		bool flag = false;
+		switch (m.type)
+		{
+		case 1: {
+			for (int i = 0; i < m.n; i++) {
+				if (m.canuse[i] == true && m.matrix[r][i] > 0 && m.visited[i] == 0 && m.label[i] == 0) {
+					s.push(i);
+					flag = true;
+					break;
+				}
+			}
+			break;
+		}
+		case 0: {
+			for (int i = 0; i < m.sz[r]; i++) {
+				int dinh = m.list[r][i];
+				if (m.canuse[dinh] == true  && m.visited[dinh] == 0 && m.label[dinh] == 0) {
+					s.push(dinh);
+					flag = true;
+					break;
+				}
+			}
+			break;
+		}
+		default:
+			break;
+		}
+		if (flag == false) {
+			s.pop();
+		}
+	}
+}
+
+void cntTPLT(Graph& m) {
+	int label = 0;
+	for (int i = 0; i < m.n; i++) {
+		if (m.label[i] == 0) {
+			label++;
+			duyet(m, i, label);
+		}
+	}
+
+	if (label == 1) {
+		cout << "Do thi lien thong \n";
+	}
+	else {
+		cout << "Do thi khong lien thong \n";
+		for (int i = 1; i <= label; i++) {
+			cout << "Thanh phan lien thong thu " << i << ": ";
+			for (int j = 0; j <m.n; j++) {
+				if (m.label[j] == i) {
+					cout << j + 1 << " ";
+				}
+			}
+			cout << endl;
+		}
+	}
+
+
+}
+
+bool checkCycleNoDir(Graph& m,int v , int& start, int& goal) {
+	for (int i = 0; i < m.n; i++) {
+		m.visited[i] = 0;
+		m.parent[i] = 0;
+	}
+	stack<int>s;
+	s.push(v);
+	while (!s.empty())
+	{
+		int r = s.top();
+		m.visited[r] = 1;
+		bool flag = false;
+		switch (m.type)
+		{
+
+		case 1: {
+			for (int i = 0; i < m.n; i++) {
+				if (m.canuse[i] == true && m.matrix[r][i] > 0 && m.visited[i] == 0) {
+					s.push(i);
+					flag = true;
+					m.parent[i] = r;
+					break;
+				}
+				else if(m.matrix[r][i] > 0 && i != m.parent[r]) {
+					start = i;
+					goal = r;
+					return true;
+				}
+			}
+			break;
+		}
+		case 0: {
+			for (int i = 0; i < m.sz[r]; i++) {
+				int dinh = m.list[r][i];
+				if (m.canuse[dinh] == true  && m.visited[dinh] == 0) {
+					s.push(dinh);
+					flag = true;
+					m.parent[dinh] = r;
+					break;
+				}
+				else if ( dinh != m.parent[r]) {
+					start = dinh;
+					goal = r;
+					return true;
+				}
+			}
+			break;
+		}
+		default:
+			break;
+		}
+		if (flag == false) {
+			s.pop();
+		}
+	}
+	return false;
+}
+
+void display(Graph m,int s , int g) {
+	stack <int>cycle;
+	int idx = m.parent[g];
+	cycle.push(g + 1);
+	while (idx!=s)
+	{
+		cycle.push(idx+1);
+		idx = m.parent[idx];
+	}
+	cycle.push(s + 1);
+	while (!cycle.empty())
+	{
+		cout << cycle.top() << " ";
+		cycle.pop();
+	}
+	cout << s + 1 << endl;
+}
+
+
+bool checkCycleDir(Graph& m,int v, int& start, int& goal) {
+	
+	stack<int>s;
+	s.push(v);
+	while (!s.empty())
+	{
+		int r = s.top();
+		m.visited[r] = 1;
+		bool flag = false;
+
+		for (int i = 0; i < m.sz[r]; i++) {
+			int dinh = m.list[r][i];
+			if (m.canuse[dinh] == true && m.visited[dinh] == 0) {
+				s.push(dinh);
+				flag = true;
+				m.parent[dinh] = r;
+				break;
+			}
+			else if (m.visited[dinh] == 1) {
+				start = dinh;
+				goal = r;
+				return true;
+			}
+		}
+
+		if (flag == false) {
+			m.visited[r] = 2;
+			s.pop();
+		}
+	}
+	return false;
+}
+
+	int main() {
+		Graph m;
+		cout << "Dung ma tran ke ( bam 1 ) / danh sach ke ( bam 0 ): ";
+		cin >> m.type;
+
+		init(m);
+		createGraph(m);
+		output(m);
+
+		if (m.type == 0) {
+			cout << "Do thi co huong ( bam 1 ) / Do thi vo huong ( bam 0 ) : ";
+			cin >> m.dir;
+		}
+
+		int c;
+		do
+		{
+			cout << "MENU \n";
+			cout << "1. Them dinh\n";
+			cout << "2. xoa dinh\n";
+			cout << "3. them canh\n";
+			cout << "4. xoa canh\n";
+			int chose;
+			cin >> chose;
+
+			switch (chose)
+			{
+			case 1: {
+				int v;
+				cout << "Nhap so dinh can them: \n";
+				cin >> v;
+
+				addVertex(m, v);
+				break;
+			}
+			case 2: {
+				int v;
+				cout << "Nhap dinh can xoa: \n";
+				cin >> v;
+				v--;
+				delVertex(m, v);
+				break;
+			}
+			case 3: {
+				int a, b;
+				cout << "Nhap canh a: ";
+				cin >> a;
+				cout << "Nhap canh b: ";
+				cin >> b;
+				a--; b--;
+				addEdge(m, a, b);
+				break;
+			}
+			case 4: {
+				int a, b;
+				cout << "Nhap canh a: ";
+				cin >> a;
+				cout << "Nhap canh b: ";
+				cin >> b;
+				a--; b--;
+				delEdge(m, a, b);
+				break;
+			}
+			default:
+				break;
+			}
+			output(m);
+			int res = isGraph(m);
+			if (res != -1) {
+				cout << "So canh cua do thi hien tai : " << res << "\n";
+			}
+			cout << "Tiep tuc ( bam 1 )/ ket thuc ( bam 0 ) : ";
+			cin >> c;
+		} while (c == 1);
+
+
+		cntTPLT(m);
+		if (m.dir == 0) {
+			for (int i = 0; i < m.n; i++) {
+				m.visited[i] = 0;
+				m.parent[i] = 0;
+			}
+			int s, g;
+			bool res=false;
+			for (int i = 0; i < m.n; i++) {
+				res = checkCycleNoDir(m,i,s,g);
+			}
+			if (res) {
+				cout << "Co chu trinh \n";
+				display(m,s,g);
+			}
+			else {
+				cout << "Khong co chu trinh \n";
+			}
+
+		}
+		else {
+			for (int i = 0; i < m.n; i++) {
+				m.visited[i] = 0;
+				m.parent[i] = 0;
+			}
+			int s, g;
+			bool res = false;
+			for (int i = 0; i < m.n; i++) {
+				res = checkCycleDir(m, i, s, g);
+			}
+			if (res) {
+				cout << "Co chu trinh \n";
+				display(m, s, g);
+			}
+			else {
+				cout << "Khong co chu trinh \n";
+			}
+		}
+
+		del(m);
+		return 0;
+	}
--- sort.cpp
+++ sort.cpp
@@ -2,107 +2,444 @@
 #include <fstream>
 #include <string>
 #include <climits>
+#define ll long long
 using namespace std;
 
-struct HoTen {
-	string *a;
+const int M = 65;
+
+struct name {
+	string s;
+	int len;
+};
+
+struct SinhVien {
+	ll mssv;
+	name hoten;
+	double dtb, hp;
+};
+
+struct List {
+	SinhVien* list;
 	int n;
 };
-void init(HoTen& s) {
-	s.a = nullptr;
-	s.n = 0;
-}
 
-void push(string*&a, int &n, string x) {
+void push(SinhVien*& a, int& n, SinhVien x) {
 	if (a == nullptr) {
 		n = 1;
-		a = new string[n + 1];
+		a = new SinhVien[n];
 		a[0] = x;
 		return;
 	}
-	string *newarr = new string[n + 1];
+	SinhVien* newarr = new SinhVien[n + 1];
 	for (int i = 0; i < n; i++) {
 		newarr[i] = a[i];
-	} 
+	}
 	newarr[n] = x;
-	delete[] a;
+	delete[]a;
 	a = newarr;
 	n++;
-
 }
 
-
-void readData(HoTen &s) {
-	ifstream fin;
-	fin.open("danhsach.txt");
-	while (!fin.eof()) {
-		string x;
-		getline(fin, x);
-		push(s.a, s.n, x);
+ll getVal(SinhVien x, int chon1) {
+	switch (chon1)
+	{
+	case 1: {
+		return x.mssv;
+		break;
+	}
+	case 2: {
+		return x.hoten.len;
+		break;
+	}
+	case 3: {
+		return x.dtb * 10;
+		break;
+	}
+	case 4: {
+		return x.hp * 100;
+		break;
 	}
 
-	fin.close();
-}
-
-void output(HoTen s) {
-	for (int i = 0; i < s.n; i++) {
-		cout << s.a[i] << endl;
+	default:
+		break;
 	}
-	
+
 }
 
-int getTen(string s) {
-	int res = 0;
-
-	for (int i = s.size()-1; i < s.size(); i--) {
-		if (s[i-1] == ' ') {
-			res = s[i] - '0';
+ll getIdx(SinhVien x) {
+	ll res = 0;
+	for (int i = x.hoten.s.length() - 1; i >= 0; i--) {
+		if (x.hoten.s[i - 1] == ' ') {
+			res = x.hoten.s[i] - '0';
 			break;
 		}
 	}
 	return res;
 }
-
-void countingSort(HoTen s,int exp) {
-	int max = 9;
-	int*count = new int[max + 1];
-	for (int i = 0; i <= max; i++) {
-		count[i] = 0;
-	}
-
-	for (int i = 0; i < s.n; i++) {
+void output(SinhVien** a , int*sz) {
+	cout << "DANH SACH HIEN TAI \n";
+	for (int i = 0; i < M; i++) {
+		if (a[i] == nullptr)continue;
+		for (int j = 0; j < sz[i]; j++) {
+			cout << a[i][j].mssv << " " << a[i][j].hoten.s << " " << a[i][j].dtb << " " << a[i][j].hp <<endl;
+		}
 		
 	}
+}
 
+int calLen(SinhVien x) {
+	int n = x.hoten.s.length();
+	int res = 0;
+	for (int i = n - 1; i >= 0; i--) {
+
+		if (x.hoten.s[i - 1] == ' ') {
+			res++;
+			break;
+		}
+		res++;
+
+	}
+	return res;
+}
+void readData(SinhVien**&a ,int *&sz) {
+	ifstream fin;
+	fin.open("SinhVien.txt");
+	while (!fin.eof())
+	{
+		SinhVien x;
+		fin >> x.mssv;
+		fin.ignore();
+		getline(fin, x.hoten.s, '#');
+		x.hoten.len = 0;
+		fin >> x.dtb;
+		fin.ignore();
+		fin >> x.hp;
+		ll idx = getIdx(x)%M;
+		push(a[idx], sz[idx], x);
+	}
+	fin.close();
+
+	
 
 }
 
-int getMaxTen(HoTen s) {
-	int max = INT_MIN;
-	for (int i = 0; i < s.n; i++) {
-		int val = getTen(s.a[i]);
-		if (val > max) {
+
+
+ll cmpMSSV(SinhVien a, SinhVien b) {
+	return a.mssv - b.mssv;
+}
+
+ll cmpTen(SinhVien a, SinhVien b) {
+	return a.hoten.len - b.hoten.len;
+}
+ll cmpDTB(SinhVien a, SinhVien b) {
+	return a.dtb - b.dtb > 0 ? 1 : a.dtb - b.dtb < 0 ? -1 : 0;
+}
+ll cmpHP(SinhVien a, SinhVien b) {
+	return a.hp - b.hp > 0 ? 1 : a.hp - b.hp < 0 ? -1 : 0;
+}
+
+void shellSort(SinhVien*& a, int n, ll func(SinhVien a, SinhVien b),int chon2) {
+	for (int gap = n / 2; gap > 0; gap /= 2) {
+		for (int i = gap; i < n; i++) {
+			SinhVien tmp = a[i];
+			int j;
+			switch (chon2)
+			{
+			case 1: {
+				for (j = i; j >= gap && func(a[j-gap],tmp) > 0; j -= gap) {
+					a[j] = a[j - gap];
+				}
+				break;
+
+			}
+			case 2: {
+				for (j = i; j >= gap && func(a[j - gap], tmp) < 0; j -= gap) {
+					a[j] = a[j - gap];
+				}
+				break;
+			}
+			default:
+				break;
+			}
+			a[j] = tmp;
+		}
+		
+	}
+
+}
+
+ll findMax(SinhVien* a, int n , int chon1) {
+	ll max = INT_MIN;
+	for (int i = 0; i < n; i++) {
+		int val = getVal(a[i], chon1);
+		if (max < val) {
 			max = val;
 		}
 	}
 	return max;
 }
 
-void radixSort(HoTen s) {
-	int max = getMaxTen(s);
-	for (int exp = 1; max / exp != 0; exp *= 10) {
-		countingSort();
+void countingSort(SinhVien*& a, int n,int chon1,int chon2, int exp = 0) {
+	ll max = 0;
+	if (exp != 0) {
+		max = 9;
 	}
+	else {
+		max = findMax(a, n, chon1);
+	}
+	int* cnt = new int[max + 1];
+	for (int i = 0; i <= max; i++) {
+		cnt[i] = 0;
+	}
+	for (int i = 0; i < n; i++) {
+		int val = exp!= 0 ? (getVal(a[i], chon1)/exp) % 10 : getVal(a[i], chon1);
+		cnt[val]++;
+	}
+
+	for (int i = 1; i <= max; i++) {
+		cnt[i] += cnt[i - 1];
+	}
+	SinhVien* output = new SinhVien[n];
+	for (int i = n-1; i >=0 ; i--) {
+		int val = exp != 0 ? (getVal(a[i], chon1) / exp) % 10 : getVal(a[i], chon1);
+		output[cnt[val] - 1] = a[i];
+		cnt[val]--;
+	}
+	
+	if (chon2 == 1 || exp !=0) {
+		for (int i = 0; i < n; i++) {
+			a[i] = output[i];
+		}
+	}
+	else if (exp==0) {
+		int idx = 0;
+		for (int i = n - 1; i >= 0; i--) {
+			a[idx] = output[i];
+			idx++;
+		}
+	}
+	delete[]output;
+	delete[]cnt;
+}
+
+void radixSort(SinhVien*& a, int n, int chon1, int chon2) {
+	ll max = findMax(a, n, chon1);
+	for (int exp = 1; max / exp != 0; exp *= 10) {
+		countingSort(a, n, chon1, chon2,exp);
+	}
+	if (chon2 == 2) {
+		for(int i = 0 ; i < n/2 ; i++){
+			swap(a[i], a[(n - 1) - i]);
+		}
+	}
+	
+}
+
+void bucketSort(SinhVien*& a, int n, int chon1, int chon2) {
+	SinhVien**bucket = new SinhVien*[n];
+	int* sz = new int[n];
+	for (int i = 0; i < n; i++) {
+		bucket[i] = nullptr;
+		sz[i] = 0;
+	}
+
+	for (int i = 0; i < n; i++) {
+		int idx = 0;
+		push(bucket[idx], sz[idx], a[i]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (bucket[i] == nullptr)continue;
+		radixSort(bucket[i], sz[i], chon1, chon2);
+	}
+
+	
+	int idx = 0;
+	/*if (chon2 == 1) {*/
+		for (int i = 0; i < n; i++) {
+			if (bucket[i] == nullptr)continue;
+			for (int j = 0; j < sz[i]; j++) {
+				a[idx] = bucket[i][j];
+				idx++;
+			}
+		}
+	/*}
+	else  {
+		for (int i = n - 1; i >= 0; i--) {
+			if (bucket[i] == nullptr)continue;
+			for (int j = sz[i]-1; j >= 0 ; j--) {
+				a[idx] = bucket[i][j];
+				idx++;
+			}
+		}
+	}*/
+
 }
 
 
+void output2(SinhVien * list, int n) {
+	cout << "DANH SACH HIEN TAI\n";
+	for (int i = 0; i < n; i++) {
+		cout << list[i].mssv << " " << list[i].hoten.s << " " << list[i].dtb << " " << list[i].hp << endl;
+	}
+}
+
+void process(SinhVien ** &a , int *&sz, int chon , int chon1, int chon2, List& l) {
+	for (int i = 0; i < M; i++) {
+		if (a[i] == nullptr)continue;
+		for (int j = 0; j < sz[i]; j++) {
+			
+			a[i][j].hoten.len = calLen(a[i][j]);
+		}
+	}
+
+	if (chon1 != 2) {
+
+		for (int i = 0; i < M; i++) {
+			if (a[i] == nullptr)continue;
+			for (int j = 0; j < sz[i]; j++) {
+				push(l.list, l.n, a[i][j]);
+			}
+		}
+
+		switch (chon)
+		{
+		case 1: {
+			switch (chon1)
+			{
+			case 1: {
+				shellSort(l.list, l.n, cmpMSSV, chon2);
+				break;
+			}
+			case 3: {
+				shellSort(l.list, l.n, cmpDTB, chon2);
+				break;
+			case 4: {
+				shellSort(l.list, l.n, cmpHP, chon2);
+				break;
+			}
+			}
+			default:
+				break;
+			}
+			break;
+		}
+		case 2: {
+			countingSort(l.list, l.n, chon1, chon2);
+			break;
+		}
+		case 3: {
+			radixSort(l.list, l.n, chon1, chon2);
+			break;
+		}
+		case 4: {
+			bucketSort(l.list, l.n, chon1, chon2);
+			break;
+		}
+		default:
+			break;
+		}
+
+	}
+	else {
+		for (int i = 0; i < M; i++) {
+			if (a[i] == nullptr)continue;
+			switch (chon)
+			{
+			case 1: {
+				shellSort(a[i], sz[i], cmpTen, chon2);
+				
+				break;
+			}
+			case 2: {
+				countingSort(a[i], sz[i], chon1, chon2);
+				break;
+			}
+			case 3: {
+				radixSort(a[i], sz[i], chon1, chon2);
+				break;
+			}
+			case 4: {
+				bucketSort(a[i], sz[i], chon1, chon2);
+				break;
+			}
+			default:
+				break;
+			}
+		}
+	}
+
+
+	
+	if (chon1 != 2) {
+		output2(l.list, l.n);
+	}
+	else {
+		output(a, sz);
+
+	}
+
+}
+
+
+
+
 int main() {
-	HoTen s;
-	init(s);
-	readData(s);
-	output(s);
+	SinhVien** a;
+	List l;
+	int* sz;
+	
+	
+	int c;
+	do
+	{
+		a = nullptr;
+		l.list = nullptr;
+		l.n = 0;
+		a = new SinhVien*[M];
+		sz = new int[M];
+		for (int i = 0; i < M; i++) {
+			a[i] = nullptr;
+			sz[i] = 0;
+		}
+		readData(a, sz);
+		output(a, sz);
+		cout << "MENU\n";
+		cout << "1. Shell sort \n";
+		cout << "2. counting sort \n";
+		cout << "3. radix sort \n";
+		cout << "4. bucket sort \n";
+		int chon;
+		cout << "Chon chuc nang: ";
+		cin >> chon;
+		cout << "MENU\n";
+		cout << "1. MSSV \n";
+		cout << "2. Ten \n";
+		cout << "3. DTB \n";
+		cout << "4. HP\n";
+		int chon1;
+		cout << "Ban muon sort theo: ";
+		cin >> chon1;
+		cout << "MENU\n";
+		cout << "1. Tang dan \n";
+		cout << "2. Giam dan\n";
+		int chon2;
+		cout << "Chon thu tu sap xep: ";
+		cin >> chon2;
+		process(a, sz, chon, chon1, chon2,l);
 
+		cout << "Tiep tuc ( bam 1 ) / ket thuc( bam 0 ): ";
+		cin >> c;
+	} while (c==1);
 
+	
 
+	for (int i = 0; i < M; i++) {
+		delete[]a[i];
+	}
+	delete[]l.list;
+	delete[]a;
+	delete[]sz;
 	return 0;
 }
